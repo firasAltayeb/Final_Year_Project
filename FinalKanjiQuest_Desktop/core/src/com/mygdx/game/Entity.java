6bdca335e72a920f9322d1bc8f549a187acfd886
firas <tayebfiras@gmail.com>
2023-07-14 23:38:22 +0100

    Part 3 of decoupling the code - make it more extendible
    Implemented a core shell class that binds the different components together and acts as the top-level container class that passes messages to all the components

---
+++ FinalKanjiQuest_Desktop/core/src/com/mygdx/game/Entity.java
@@ -0,0 +1,144 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
+import java.util.ArrayList;
+/*
+public class Entity {
+	private static final String TAG = Entity.class.getSimpleName();
+	private Json json;
+	private EntityConfig entityConfig;
+
+	public static enum Direction {
+		UP,
+		RIGHT,
+		DOWN,
+		LEFT;
+
+		static public Direction getRandomNext() {
+			return Direction.values()[MathUtils.random(Direction.values().length - 1)];
+		}
+
+		public Direction getOpposite() {
+			if( this == LEFT){
+				return RIGHT;
+			}else if( this == RIGHT){
+				return LEFT;
+			}else if( this == UP){
+				return DOWN;
+			}else{
+				return UP;
+			}
+		}
+	}
+
+	public static enum State {
+		IDLE,
+		WALKING,
+
+		IMMOBILE;//This should always be last
+
+		static public State getRandomNext() {
+			//Ignore IMMOBILE which should be last state
+			return State.values()[MathUtils.random(State.values().length - 2)];
+		}
+	}
+
+	public static enum AnimationType {
+		WALK_LEFT,
+		WALK_RIGHT,
+		WALK_UP,
+		WALK_DOWN,
+		IDLE,
+		IMMOBILE
+	}
+
+	public static final int FRAME_WIDTH = 16;
+	public static final int FRAME_HEIGHT = 16;
+
+	private static final int MAX_COMPONENTS = 5;
+	private Array<Component> components;
+
+	private InputComponent inputComponent;
+	private GraphicsComponent graphicsComponent;
+	private PhysicsComponent physicsComponent;
+
+	public Entity(InputComponent inputComponent, PhysicsComponent physicsComponent, GraphicsComponent graphicsComponent){
+		entityConfig = new EntityConfig();
+		json = new Json();
+
+		components = new Array<Component>(MAX_COMPONENTS);
+
+		this.inputComponent = inputComponent;
+		this.physicsComponent = physicsComponent;
+		this.graphicsComponent = graphicsComponent;
+
+		components.add(this.inputComponent);
+		components.add(this.physicsComponent);
+		components.add(this.graphicsComponent);
+	}
+
+	public EntityConfig getEntityConfig() {
+		return entityConfig;
+	}
+
+	public void sendMessage(Component.MESSAGE messageType, String ... args){
+		String fullMessage = messageType.toString();
+
+		for (String string : args) {
+			fullMessage += Component.MESSAGE_TOKEN + string;
+		}
+
+		for(Component component: components){
+			component.receiveMessage(fullMessage);
+		}
+	}
+
+	public void update(MapManager mapMgr, Batch batch, float delta){
+		inputComponent.update(this, delta);
+		physicsComponent.update(this, mapMgr, delta);
+		graphicsComponent.update(this, mapMgr, batch, delta);
+	}
+
+	public void dispose(){
+		for(Component component: components){
+			component.dispose();
+		}
+	}
+
+	public Rectangle getCurrentBoundingBox(){
+		return physicsComponent.boundingBox;
+	}
+
+	public void setEntityConfig(EntityConfig entityConfig){
+		this.entityConfig = entityConfig;
+	}
+
+	//link the json file path with its POJO
+	public static EntityConfig getEntityConfig(String configFilePath){
+		Json json = new Json();
+		return json.fromJson(EntityConfig.class, Gdx.files.internal(configFilePath));
+	}
+
+	public static Array<EntityConfig> getEntityConfigs(String configFilePath){
+		Json json = new Json();
+		//array of entity configs
+		Array<EntityConfig> configs = new Array<EntityConfig>();
+
+		//link a jason file with the each POJO
+    	ArrayList<JsonValue> list = json.fromJson(ArrayList.class, Gdx.files.internal(configFilePath));
+
+		for (JsonValue jsonVal : list) {
+			configs.add(json.readValue(EntityConfig.class, jsonVal));
+		}
+
+		return configs;
+	}
+
+}
+*/
